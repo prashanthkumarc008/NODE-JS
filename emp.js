c8124fbed409b772c98e725193b59a6135036665
prashanthkumarc008 <prashanthkumarc008@gmail.com>
2025-07-02 16:40:52 +0530

    changed

---
+++ emp.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+const data=fs.readFile(path.join(process.cwd(),"emp_data","data","emp","emp.json"),'utf-8',(err,data)=>{
+    if(err) throw err
+    console.log(typeof(data));
+    let emp=JSON.parse(data);
+    for(emp of emp){
+        console.log(emp.name);
+    }
+})
